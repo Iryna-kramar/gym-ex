1f978161e175eab1e2b67c525ff80f209fc10c2a
Iryna Kramar <irynakramar@Irynas-MacBook-Air.local>
2022-08-04 23:19:52 +0300

    Fetching API

---
+++ src/utils/fetchData.js
@@ -0,0 +1,16 @@
+export const exerciseOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+  },
+};
+
+// url: "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+
+export const fetchData = async (url, options) => {
+  const response = await fetch(url, options);
+  const data = await response.json();
+
+  return data;
+};
